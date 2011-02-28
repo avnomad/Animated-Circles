fdf7cc382a657085ff6f28362693c6c54b44540c
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2011-02-28 14:38:37 +0200

    comparing line segments defined with different weights. unequal weights seem to affect the spacing of evaluated points.
    --HG--
    branch : static|curves

--- animation/main.cpp
+++ animation/main.cpp
@@ -7,6 +7,8 @@ using std::clog;
 using std::left;
 
 #include <cmath>
+#include <algorithm>
+using std::min;
 
 #include <GL/glew.h>
 #include <GL/glut.h>
@@ -16,24 +18,51 @@ using std::left;
 
 void display()
 {
-	float curve[][4] = {{1,0,0,1},{0.70711,0.70711,0,0.70711},{0,1,0,1}};
+	float circle[][4] = {{1,0,0,1},{0.70711,0.70711,0,0.70711},{0,1,0,1}};
+	float line1[][4] = {{0,0,0,1},{0.35355,0,0,0.70711},{1,0,0,1}};
+	float line2[][4] = {{0,0,0,1},{0.5,0,0,1},{1,0,0,1}};
+
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glLoadIdentity();
-	glTranslatef(50,50,0);
-	glScalef(100,100,0);
+	// draw circle quadrant with rational Bezier
+	glPushMatrix();
+		glTranslatef(45,45,0);
+		glScalef(50,50,0);
 
-	glMap1f(GL_MAP1_VERTEX_4,0.0,1.0,4,3,&curve[0][0]);
-	glEvalMesh1(GL_POINT,0,SEGMENTS);
+		glMap1f(GL_MAP1_VERTEX_4,0.0,1.0,4,3,&circle[0][0]);
+		glEvalMesh1(GL_LINE,0,SEGMENTS);
+	glPopMatrix();
 
-	glLoadIdentity();
-	glTranslatef(45,45,0);
-	glScalef(100,100,0);
+	// again with sine/cosine
+	glPushMatrix();
+		glTranslatef(40,40,0);
+		glScalef(50,50,0);
 
-	glBegin(GL_POINTS);
-		for(int i = 0 ; i <= SEGMENTS ; ++i)
-			glVertex2f(cos(PI/2*i/SEGMENTS),sin(PI/2*i/SEGMENTS));
-	glEnd();
+		glBegin(GL_LINE_STRIP);
+			for(int i = 0 ; i <= SEGMENTS ; ++i)
+				glVertex2f(cos(PI/2*i/SEGMENTS),sin(PI/2*i/SEGMENTS));
+		glEnd();
+	glPopMatrix();
+
+	glDisable(GL_MULTISAMPLE);
+	// draw straight line with rational Bezier and same weights as circle
+	glPushMatrix();
+		glTranslatef(40,35,0);
+		glScalef(50,50,0);
+
+		glMap1f(GL_MAP1_VERTEX_4,0.0,1.0,4,3,&line1[0][0]);
+		glEvalMesh1(GL_POINT,0,SEGMENTS);
+	glPopMatrix();
+
+	// draw straight line with rational Bezier and unit weights
+	glPushMatrix();
+		glTranslatef(40,30,0);
+		glScalef(50,50,0);
+
+		glMap1f(GL_MAP1_VERTEX_4,0.0,1.0,4,3,&line2[0][0]);
+		glEvalMesh1(GL_POINT,0,SEGMENTS);
+	glPopMatrix();
+	glEnable(GL_MULTISAMPLE);
 
 	glutPostRedisplay();
 	glutSwapBuffers();
@@ -52,11 +81,7 @@ void keyboard(unsigned char key, int x, int y)
 
 void reshape(int w, int h)
 {
-	glViewport(0,0,w,h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0,w,0,h);
-	glMatrixMode(GL_MODELVIEW);
+	glViewport(0,0,min(w,h),min(w,h));
 } // end function reshape
 
 
@@ -64,7 +89,7 @@ int main(int argc, char **argv)
 {
 	// glut initialization
 	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
 	glutInitWindowSize(640,640);
 	glutInitWindowPosition(620,160);
 	glutCreateWindow("animated circles");
@@ -73,7 +98,9 @@ int main(int argc, char **argv)
 	glewInit();
 
 	// OpenGL initialization
-	glEnable(GL_BLEND);
+	glMatrixMode(GL_PROJECTION);
+		gluOrtho2D(0,100,0,100);
+	glMatrixMode(GL_MODELVIEW);
 	glColor3f(1.0,0.75,0.0);
 	glEnable(GL_MAP1_VERTEX_4);
 	glMapGrid1f(SEGMENTS,0.0,1.0);
